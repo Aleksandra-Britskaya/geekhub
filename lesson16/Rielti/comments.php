b32c07d9216a82d89de0f34fe16a24fe0959b6ca
Abrikoska <zarri@ukr.net>
2013-02-19 17:59:38 +0200

    correct lesson 16

--- lesson16/Rielti/comments.php
+++ lesson16/Rielti/comments.php
@@ -1,8 +1,29 @@
-<?php ?>
 <div id="comments">
-	<?php if ( post_password_required() ) : ?>
-		<p class="nopassword"><?php _e( 'This post is password protected. Enter the password to view any comments.', 'twentyeleven' ); ?></p>
-	</div><!-- #comments -->
-	<?php return;
-		endif;
-	?>
+	<h3><?php comments_number('Нет комментариев', '1 Комментарий', '% Комментариев' );?></h3>
+	<?php if ( comments_open() ) : ?>
+		<div id="respond">
+			<h3>Оставить комментарий</h3>
+				<form action="<?php echo get_option('siteurl'); ?>">
+					<fieldset>
+						<label for="author">Имя: </label>
+						<input type="text" name="author" id="author" value="<?php echo $comment_author; ?>" />
+						
+						<label for="email">Email: </label>
+						<input type="text" name="email" id="email" value="<?php echo $comment_author_email; ?>" />
+						
+						<label for="comment">Сообщение: </label>
+						<textarea name="comment" id="comment" rows="" cols=""></textarea>
+						
+						<input type="submit" class="commentsubmit" value="Добавить комментарий" />
+			
+						<?php comment_id_fields(); ?>
+						<?php do_action('comment_form', $post->ID); ?>
+			
+					</fieldset>
+		</div>
+		
+	<?php endif; ?>
+
+
+	
+	
